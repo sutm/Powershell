f459f15c4d974fd547ffa1515181a173d9e86a7d
Tun Miang Su <Tun Miang Su>
2015-11-26 23:08:34 +0800

    initial add

---
+++ network.ps1
@@ -0,0 +1,17 @@
+function Get-InfoNIC {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $nics = Get-WmiObject -class Win32_NetworkAdapter -ComputerName $ComputerName `
+           -Filter "PhysicalAdapter=True"
+    foreach ($nic in $nics) {      
+        $props = @{'NICName'=$nic.servicename;
+                   'Speed'=$nic.speed / 1MB -as [int];
+                   'Manufacturer'=$nic.manufacturer;
+                   'MACAddress'=$nic.macaddress;
+                   'AdapterType'=$nic.AdapterType}
+        New-Object -TypeName PSObject -Property $props
+    }
+}
+
